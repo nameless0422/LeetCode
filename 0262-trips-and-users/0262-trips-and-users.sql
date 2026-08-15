4224361375fcc8559f6af78ff813743d51a31571
휴우。 <46670103+nameless0422@users.noreply.github.com>
2026-08-16 02:22:51 +0900

    Time: 611 ms (48.91%), Space: 0B (100%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
-select request_at as Day, round((sum(case when status like 'cancelled%' then 1 else 0 end)/count(*)),2) as 'Cancellation Rate'
-from Trips T 
-join Users C on T.client_id = C.users_id and C.banned ='No'
-join Users D on T.driver_id = D.users_id and D.banned ='No'
-where T.request_at between '2013-10-01' and '2013-10-03'
-group by T.request_at
+select request_at Day, round(avg(status != 1), 2) 'Cancellation Rate'
+from Trips
+where request_at between '2013-10-01' and '2013-10-03'
+  and not exists (select 1 from Users where banned = 'Yes' and users_id in (client_id, driver_id))
+group by 1
